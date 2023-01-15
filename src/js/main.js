511be00a7cdcff6b2241d2772507145c3e76249a
Konstantin <kosvadmail@atomlc.com.ua>
2023-01-15 17:03:38 +0100

    Добавляет фундамент проекта

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+import '../sass/style.scss';
+// import { startAnimation } from './animation';
+// import './scrollReveal-animation';
+
+// startAnimation();
---
+++ src/js/scrollReveal-animation.js
@@ -0,0 +1,2 @@
+// import ScrollReveal from 'scrollreveal';
+// const mediaQuery1024 = window.matchMedia('(min-width: 1024px)');
