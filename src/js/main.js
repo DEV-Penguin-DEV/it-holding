a22956508e80ae65af89f0d4817fb840dac82c97
Konstantin <kosvadmail@atomlc.com.ua>
2023-01-27 14:54:14 +0100

    Добавляет остаток проекта(комп версию и анимацию)

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 import '../sass/style.scss';
 // import { startAnimation } from './animation';
-// import './scrollReveal-animation';
+import './scrollReveal-animation';
 
 // startAnimation();
--- src/js/scrollReveal-animation.js
+++ src/js/scrollReveal-animation.js
@@ -1,2 +1,31 @@
-// import ScrollReveal from 'scrollreveal';
+import ScrollReveal from 'scrollreveal';
 // const mediaQuery1024 = window.matchMedia('(min-width: 1024px)');
+
+const move = (elem, origin, distance = '250px', duration = 1200) => {
+  ScrollReveal().reveal(elem, {
+    reset: true,
+    distance: distance,
+    origin: origin,
+    duration: duration,
+  });
+};
+
+const scale = (elem, duration = 1500) => {
+  ScrollReveal().reveal(elem, {
+    reset: true,
+    scale: 1.4,
+    duration: duration,
+    distance: '0px',
+  });
+};
+
+move('.first-screen__title', 'left');
+move('.first-screen__undertitle-text', 'bottom');
+move('.first-screen__text', 'left');
+move('.button', 'bottom', '100px');
+scale('.first-screen__decor-container', 1700);
+move('.title', 'left');
+move('.text', 'right');
+move('.portfolio__item', 'top', '350px', 1500);
+scale('.product', 1700);
+move('.products__bg', 'left');
